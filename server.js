e63d69f923d4d484c2ac3a1b4f6f921ec4c29e36
Gökmen <67012216+Gashlax@users.noreply.github.com>
2021-02-12 10:22:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db =knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-rigid-47623',
     user : 'postgres',
     password : '2112131',
     database : 'smart-brain'
