eab90a7a9df1b226e249ba36237dd5497bf7937d
Gökmen <67012216+Gashlax@users.noreply.github.com>
2021-02-12 10:28:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db =knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-rigid-47623',
-    user : 'postgres',
-    password : '2112131',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
